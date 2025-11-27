1e5cb8e67e57e83878eb81cf3bd29ea94206aac3
Dj <Beahmer89@gmail.com>
2025-11-26 16:22:04 -0800

    Add new tables to keep meal planning outline

--- db_setup.py
+++ db_setup.py
@@ -5,10 +5,10 @@ CONNECTION = duckdb.connect(database="./my-db.duckdb")
 
 
 def main():
-    CONNECTION.execute("CREATE SEQUENCE recipes_id_seq START 1 INCREMENT 1;")
+    CONNECTION.execute("CREATE SEQUENCE IF NOT EXISTS recipes_id_seq START 1 INCREMENT 1;")
     # create a table
     CONNECTION.execute(
-        """CREATE TABLE recipes (
+        """CREATE TABLE IF NOT EXISTS recipes (
         id INTEGER PRIMARY KEY DEFAULT(nextval('recipes_id_seq')),
         title TEXT,
         image TEXT,
@@ -16,6 +16,31 @@ def main():
         recipe_html TEXT,
         rating INTEGER,
         times_made INTEGER)
+        """
+    )
+
+    CONNECTION.execute("CREATE SEQUENCE IF NOT EXISTS meal_plan_id_seq START 1 INCREMENT 1;")
+    CONNECTION.execute("CREATE SEQUENCE IF NOT EXISTS meal_plan_recipe_id_seq START 1 INCREMENT 1;")
+
+    CONNECTION.execute(
+        """CREATE TABLE IF NOT EXISTS meal_plan(
+        id INTEGER PRIMARY KEY DEFAULT(nextval('meal_plan_id_seq')),
+        name TEXT,
+        status TEXT,
+        created_at TIMESTAMP WITH TIME ZONE,
+        updated_at TIMESTAMP WITH TIME ZONE,
+        )
+        """
+    )
+
+    CONNECTION.execute(
+        """CREATE TABLE IF NOT EXISTS recipe_meal_plan(
+        id INTEGER PRIMARY KEY DEFAULT(nextval('meal_plan_recipe_id_seq')),
+        meal_plan_id INTEGER REFERENCES meal_plan,
+        recipe_id INTEGER REFERENCES recipes,
+        assigned_day TEXT,
+        recipe_type TEXT
+        )
         """
     )
 
